831d58c536b9571ee657139cca3562277876a0c2
Byter <bryanchacaliazasaavedra@gmail.com>
2024-10-11 12:22:29 -0500

    iagenes-terror

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import fulldev from 'fulldev-ui/integration'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output:'server',
+  integrations: [tailwind()]
+});
---
+++ src/models/topics.ts
@@ -0,0 +1,93 @@
+export const TOPICS = {
+    graveyard_fog: {
+      name: "Cementerio con niebla",
+      icon: "ti ti-grave",
+      description: "Add twisted trees moss covered tombstones a cloudy sky and thick fog to the background",
+    },
+    haunted_house: {
+      name: "Casa encantada",
+      icon: "ti haunted-house",
+      description: "Add a decayed mansion broken windows and bats flying under a stormy sky to the background",
+    },
+    monster_cave: {
+      name: "Cueva de monstruos",
+      icon: "ti monster-cave",
+      description: "Add a dark cave with glowing eyes and strange creatures lurking to the background",
+    },
+    lab_experiment: {
+      name: "Laboratorio de experimentos",
+      icon: "ti lab-experiment",
+      description: "Add an underground lab with strange jars machines and a Frankenstein like creature to the background",
+    },
+    invasion_city: {
+      name: "Ciudad de invasión",
+      icon: "ti invasion-city",
+      description: "Add destroyed buildings with giant monsters attacking under a full moon to the background",
+    },
+    swamp_creature: {
+      name: "Criatura del pantano",
+      icon: "ti swamp-creature",
+      description: "Add a muddy swamp with reptilian creatures and thick fog to the background",
+    },
+    coliseum_battle: {
+      name: "Batalla en el coliseo",
+      icon: "ti coliseum-battle",
+      description: "Add a ruined coliseum with ghosts watching monsters fight to the background",
+    },
+    hospital_silent_hill: {
+      name: "Hospital Silent Hill",
+      icon: "ti ti-hospital",
+      description: "Add a decayed hospital with overturned gurneys and crawling figures to the background",
+    },
+    mansion_resident_evil: {
+      name: "Mansión Resident Evil",
+      icon: "ti mansion-resident-evil",
+      description: "Agrega zombies y una mansion embrujada para hallowen realista en el fondo",
+    },
+    space_station: {
+      name: "Estación espacial",
+      icon: "ti space-station",
+      description: "Add a dark space station with flickering lights and a stalking Xenomorph to the background",
+    },
+    underground_lab: {
+      name: "Laboratorio subterráneo",
+      icon: "ti underground-lab",
+      description: "Add a secret lab with test tubes and mutant creatures to the background",
+    },
+    desolate_alley: {
+      name: "Calle desolada",
+      icon: "ti desolate-alley",
+      description: "Add foggy streets with ruined buildings and spectral figures to the background",
+    },
+    haunted_forest: {
+      name: "Bosque encantado",
+      icon: "ti haunted-forest",
+      description: "Add a forest with leafless trees and ghosts floating among Jackolanterns to the background",
+    },
+    ghost_town: {
+      name: "Pueblo fantasma",
+      icon: "ti ghost-town",
+      description: "Add empty streets with destroyed houses and shadows lurking to the background",
+    },
+    pumpkin_party: {
+      name: "Fiesta de calabazas",
+      icon: "ti pumpkin-party",
+      description: "Add a moonlit field with carved pumpkins and ghosts around a bonfire to the background",
+    },
+    zombie: {
+      name: "Zombis",
+      icon: "ti zombie",
+      description: "Add zombies to the background",
+    },
+    vampire: {
+      name: "Vampiros",
+      icon: "ti vampire",
+      description: "Add vampires to the background",
+    },
+    custom: {
+      name: "Personalizado",
+      icon: "ti custom",
+      description: "",
+    },
+  } as const;
+  
